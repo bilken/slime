4a37c04b538134d8332007b22cfc649b7642f0c6
Billy Kennemer <eyepits@gmail.com>
2014-01-12 11:51:41 -0800

    Rotate around the base

--- ani.py
+++ ani.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 
-import pygame, sys, json
+import pygame, sys, json, math
 from pygame.locals import *
 from random import randint
-
-# ani format
-#   "start": {"x":0, "y":0},
-#   "end":  {"x":100, "y":10},
-#   "axis": {"x":0, "y":0},
 
 if len(sys.argv) != 3:
     print 'Missing arguments: yourImage.png yourAnimation.ani'
@@ -23,9 +18,7 @@ screenh = 480
 screen = pygame.display.set_mode((screenw, screenh))
 pygame.display.set_caption("ani")
 
-bg = pygame.image.load("pic/bg.png")
-img = pygame.image.load(imgName)#.convert()
-#ani = json.load(open(aniName))
+img = pygame.image.load(imgName).convert_alpha()
 ani = json.load(open(aniName))
 print ani
 
@@ -35,12 +28,15 @@ print 'Item size (%u, %u)' % (iw, ih)
 
 clock = pygame.time.Clock()
 
-x = (screenw - iw) / 2
-y = (screenh - ih) / 2
+sx = ani['base']['x']
+sy = ani['base']['y']
+
+ex = ani['tip']['x']
+ey = ani['tip']['y']
+
+i = len(ani['sequence'])
+
 r = 0
-
-i = 0
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,25 +44,44 @@ while 1:
 
     if i >= len(ani['sequence']):
         i = 0
-        x = (screenw - iw) / 2
-        y = (screenh - ih) / 2
-        r = 0
 
-    #screen.fill((0,0,0))
-    screen.blit(bg, [0,0])
-    if r != 0:
-        nimg = pygame.transform.rotate(img, r)
-        screen.blit(nimg, [x, y])
-    else:
-        screen.blit(img, [x, y])
+    #action = ani['sequence'][i]
+    #r += math.degrees(math.atan2(dy, dx))
+
+    r = r + 5
+
+    screen.fill((50,50,50))
+
+    brw = 2 * sx
+    brx = 0
+    if brw < iw:
+        brw = 2 * ((iw / 2) - sx)
+        brx = brw / 2 + sx
+    brh = 2 * sy
+    bry = 0
+    if brh < ih:
+        brh = 2 * ((ih / 2) - sy)
+        bry = brh / 2 + sy
+    br = pygame.Surface((brw, brh)).convert_alpha()
+    br.fill((0,0,0,0))
+    br.blit(img, (brx, bry))
+
+    brmx = brw / 2
+    brmy = brh / 2
+
+    center = [screenw / 2, screenh / 2]
+
+    rimg = pygame.transform.rotate(br, r)
+    rect = rimg.get_rect()
+    rect.center = center
+
+    screen.blit(rimg, rect)
+
+    pygame.draw.rect(screen, (200, 200, 200), (screenw / 2 - 4, screenh / 2 - 4, 8, 8))
+
     pygame.display.flip()
 
-    clock.tick(2)
-
-    action = ani['sequence'][i]
-    x += action['dx']
-    y += action['dy']
-    r += action['dr']
+    clock.tick(30)
 
     i = i + 1
 
